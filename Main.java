47c2da716bd1270db617e4c2f38003e10a9016aa
Nick <nickjohnschantz@gmail.com>
2018-01-22 21:16:24 -0500

    Finished calculator

--- Main.java
+++ Main.java
@@ -7,10 +7,10 @@ public class Main {
         Scanner input = new Scanner(System.in);
         String program = new String();
 
-        System.out.println("What program do you want to run? (Case sensitive input)");
-        program = input.next();
+       // System.out.println("What program do you want to run? (Case sensitive input)");
+       //5 program = input.next();
 
-        fibonacciSet(25);
+        calculator();
 
     }
 
@@ -47,48 +47,52 @@ public class Main {
     }
 
     public static void calculator(){
-        Scanner operationSelection = new Scanner(System.in);
+        Scanner selection = new Scanner(System.in);
         String operationName = new String();
+        int num1 = 0;
+        int num2 = 0;
 
-        System.out.println("What operation do you want to perform: Addition, Subtraction, Multiplication, Division");
-        operationName = operationSelection.next();
-        operationName.toLowerCase();
+        System.out.println("Enter number one into calculator");
+        num1 = selection.nextInt();
+
+        System.out.println("Enter number two into calculator");
+        num2 = selection.nextInt();
+
+        System.out.println("What operation do you want to perform: Addition (A), Subtraction (S), Multiplication (M), Division (D)");
+        operationName = selection.next();
+        operationName = operationName.toLowerCase();
 
         switch(operationName){
-            case "addition":
-                calcAddition();
+            case "addition": case "a":
+                System.out.println(num1 + " + " + num2 + " = " + calcAddition(num1, num2));
                 break;
-            case "subtraction":
-                calcSubtraction();
+            case "subtraction": case "s":
+                System.out.println(num1 + " - " + num2 + " = " + calcSubtraction(num1, num2));
                 break;
-            case "multiplication":
-                calcMultiplication();
+            case "multiplication": case "m":
+                System.out.println(num1 + " * " + num2 + " = " + calcMultiplication(num1, num2));
                 break;
-            case "division":
-                calcDivision();
+            case "division": case "d":
+                System.out.println(num1 + " / " + num2 + " = " + calcDivision(num1, num2) + " (Rounded)5");
                 break;
             default:
                 System.out.println("That is not an operation.");
         }
     }
 
-    public static void calcAddition(){
-        Scanner s = new Scanner(System.in);
-
+    public static int calcAddition(int num1, int num2){
+        return num1 + num2;
     }
 
-    public static void calcSubtraction(){
-        Scanner s = new Scanner(System.in);
-
+    public static int calcSubtraction(int num1, int num2){
+        return num1 - num2;
     }
 
-    public static void calcMultiplication(){
-        Scanner s = new Scanner(System.in);
-
+    public static int calcMultiplication(int num1, int num2){
+        return num1 * num2;
     }
 
-    public static void calcDivision(){
-        Scanner s = new Scanner(System.in);
-
+    public static int calcDivision(int num1, int num2){
+        return (int) ((num1 / num2) + .5);
     }
 }
